85e0c103b26c70c506893fe51a213135de87db9d
Ash Bhat <ash.bhat@getathelas.com>
2024-12-25 12:13:58 -0800

    added cors supprot

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from fastapi import FastAPI
 from sample.controllers import router as sample_router
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Replace with your domain in production
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app = FastAPI()
 app.include_router(sample_router)
