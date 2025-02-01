81e5998bd8db8988cbfed9afd80231d45ca482f3
Ash Bhat <me@ashbhat.com>
2025-02-01 14:25:03 -0800

    cleaned up router

--- api/router.py
+++ api/router.py
@@ -4,8 +4,4 @@ from api.sample.controllers import router as sample_router
 api_router = APIRouter()
 
 # Include all sub-routers
-api_router.include_router(
-    sample_router,
-    prefix="/sample",
-    tags=["sample"]
-)
+api_router.include_router(sample_router)
